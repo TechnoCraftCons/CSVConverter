5d3602d5b84121d98cd140504be24a5a3a7cd29e
TechnoCraftCons <44950527+TechnoCraftCons@users.noreply.github.com>
2020-08-19 21:47:08 -0500

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string delimiter = ",";
+
+            XDocument.Load(@"C:\Users\nebiy\source\repos\ConsoleApp1\ConsoleApp1\XMLFile1.xml").Descendants("Item").ToList().ForEach(
+                element => sb.Append(
+                                     element.Element("SNO").Value + delimiter +
+                                     element.Element("DELV_DATE").Value + delimiter +
+                                      element.Element("STORE_CD").Value + delimiter +
+                                     element.Element("ITEM_CD").Value + delimiter +
+                                     element.Element("ITEM_LONG_NAME").Value + delimiter +
+                                     element.Element("BARCODE_NO").Value + delimiter +
+                                      element.Element("ORD_SLIP_NO").Value + delimiter +
+                                     element.Element("PSA_CD").Value + delimiter +
+                                     element.Element("ORD_QTY").Value + delimiter +
+                                     element.Element("ITEM_UNIT_COST").Value + delimiter +
+                                     element.Element("ITEM_UNIT_PRICE").Value + "\r\n"));
+
+            StreamWriter sw = new StreamWriter(@"C:\Users\nebiy\source\repos\ConsoleApp1\ConsoleApp1\Result.csv");
+            sw.WriteLine(sb.ToString());
+            sw.Close();
+
+          
+        }
+    }
+}
